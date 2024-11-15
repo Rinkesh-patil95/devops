973a0bba1a48038bd259feed571a524cc35fcf33
Rinkesh-patil95 <vaibhavpati95711@gmail.com>
2024-11-15 10:04:26 +0530

    first commit

---
+++ myfirst.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Print a message
+        System.out.println("Hello, World!");
+
+        // Simple addition for testing
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
